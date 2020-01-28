6be194c9153a25f39390e1c54fd4e885ff7f7c5f
Enriquez Garcia Juan Antonio (IFAG DES TCP EDS) <JuanAntonio.EnriquezGarcia@infineon.com>
2020-01-28 15:00:31 +0100

    Updated makefile for wiced-sdk hall switch app example

--- src/pal/cypress/wiced-43xxx/hall_switch.mk
+++ src/pal/cypress/wiced-43xxx/hall_switch.mk
@@ -1,10 +1,12 @@
 NAME := App_hall_switch
 
-GLOBAL_DEFINES     := HALL_SWITCH_WICED_PLATFORM_ENABLED
+GLOBAL_DEFINES   := HALL_SWITCH_WICED_PLATFORM_ENABLED
 
 $(NAME)_INCLUDES :=  hall-switch/src/common 								\
-					 hall-switch/pal/cypress/wiced-43xxx
+					 hall-switch/src/pal/cypress/wiced-43xxx
 
 $(NAME)_SOURCES := 	hall_switch.cpp													\
 					hall-switch/src/common/hall-switch.cpp						    \
-					hall-switch/pal/cypress/wiced-43xxx/hall-switch-wiced.cpp	
+					hall-switch/src/pal/cypress/wiced-43xxx/hall-switch-wiced.cpp	
+
+VALID_PLATFORMS	:= CYW943907AEVAL1F
