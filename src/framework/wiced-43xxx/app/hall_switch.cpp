2413d92576b20107cda3c66adfa4ad754e73bfc2
Enriquez Garcia Juan Antonio (IFAG DES TCP EDS) <JuanAntonio.EnriquezGarcia@infineon.com>
2020-07-29 17:41:55 +0200

    Removed wiced include

--- src/framework/wiced-43xxx/app/hall_switch.cpp
+++ src/framework/wiced-43xxx/app/hall_switch.cpp
@@ -4,6 +4,8 @@
  * @date        May 2020
  * @copyright   Copyright (c) 2019-2020 Infineon Technologies AG
  */
+ 
+#include "../../../config/hall-conf.hpp"
 
 #if (HALL_SWITCH_FRAMEWORK == HALL_SWITCH_FRMWK_WICED)
 
